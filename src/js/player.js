f958691ea935f5a131816030d75800bc7fbeaeda
Lisa Fink <lisadfink@gmail.com>
2022-05-29 20:42:40 -0400

    Added player and ai that chooses random attacks

--- src/js/player.js
+++ src/js/player.js
@@ -6,11 +6,12 @@ const createPlayer = (name = 'Larry') => {
 
   return {
     board: createGameboard(),
-    setOpponent(opp) {
-      ai = opp;
+    setOpponent(obj) {
+      ai = obj;
     },
     attack(coordinates) {
       ai.board.receiveAttack(coordinates);
+      ai.turn = true;
     },
   };
 };
@@ -18,13 +19,31 @@ const createPlayer = (name = 'Larry') => {
 const createAi = () => {
   let player;
   const prevMoves = [];
+  const getAttackCoordinates = () => {
+    const possibleMoves = [];
+    for (let i = 0; i < 10; i++) {
+      for (let j = 0; j < 10; j++) {
+        const checkValid = prevMoves.filter((move) => {
+          let [x, y] = move;
+          return x == i && y == j;
+        });
+        !checkValid.length ? possibleMoves.push([i, j]) : null;
+      }
+    }
+    const NumChoices = possibleMoves.length;
+    const choice = Math.floor(Math.random() * NumChoices);
+    return possibleMoves[choice];
+  };
   return {
-    setPlayer(pl) {
-      player = pl;
+    setPlayer(obj) {
+      player = obj;
     },
     board: createGameboard(),
     attack() {
-      player.board.receiveAttack(getAttackCoordinates());
+      const attackCoordinates = getAttackCoordinates();
+      player.board.receiveAttack(attackCoordinates);
+      prevMoves.push(attackCoordinates);
+      this.turn = false;
     },
   };
 };
--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -1,6 +1,6 @@
 import { createPlayer, createAi } from '../js/player';
 
-test('player attack', () => {
+test('player attack sends function', () => {
   const player = createPlayer();
   const ai = createAi();
 
@@ -12,7 +12,39 @@ test('player attack', () => {
   expect(mockFunction).toHaveBeenCalled();
 });
 
-test('getAttackCoordinates', () => {
-  ai = createAi();
+test('ai attack sends function', () => {
+  const ai = createAi();
+  const player = createPlayer();
+  ai.setPlayer(player);
   ai.attack();
+
+  const mockFunction = jest.fn();
+  player.board.receiveAttack = mockFunction;
+  ai.attack();
+  expect(mockFunction).toHaveBeenCalled();
+});
+
+test('getAttackCoordinates functions correctly on smaller mock function', () => {
+  const mockPrevMoves = [
+    [0, 0],
+    [0, 1],
+    [1, 0],
+  ];
+  const mockGetAttackCoordinates = () => {
+    const mockPossibleMoves = [];
+    for (let i = 0; i < 2; i++) {
+      for (let j = 0; j < 2; j++) {
+        const checkValid = mockPrevMoves.filter((move) => {
+          let [x, y] = move;
+          return x == i && y == j;
+        });
+        !checkValid.length ? mockPossibleMoves.push([i, j]) : null;
+      }
+    }
+    const NumChoices = mockPossibleMoves.length;
+    const choice = Math.floor(Math.random() * NumChoices);
+    return mockPossibleMoves[choice];
+  };
+
+  expect(mockGetAttackCoordinates()).toEqual([1, 1]);
 });
